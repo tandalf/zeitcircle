c21be1c2429d79966eb595579c48a83d8bfb1342
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-09-14 18:54:28 +0100

    .

--- .now/github.js
+++ .now/github.js
@@ -1,5 +1,6 @@
 // const clipboardy = require('clipboardy')
 
 const NOW_URL = process.env.NOW_URL
+const CIRCLE_PULL_REQUEST = process.env.CIRCLE_PULL_REQUEST
 
-console.log('now variable', NOW_URL)
+console.log('now variables\n\n', NOW_URL, CIRCLE_PULL_REQUEST)
